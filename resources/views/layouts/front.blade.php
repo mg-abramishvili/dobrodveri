dc82507b6a429b22c93cb04cc8b32e048ff15631
Mikhail Abramishvili <mg@abramishvili.net>
2023-11-09 18:05:30 +0400

    .

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -12,7 +12,64 @@
     <body>
         <div id="front" class="wrapper">
             <header>
-                
+                <div class="header-top">
+                    <div class="container">
+                        <div class="header-top-logo">
+                            <a href="/">logo</a>
+                        </div>
+                        <div class="header-top-nav">
+                            <nav>
+                                <ul>
+                                    <li>
+                                        <a href="#">Поиск</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Акции</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Избранное</a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                        <div class="header-top-tel">
+                            <a href="#">8 937 850 40 40</a>
+                            <span>Ежедневно с 10:00 до 20:00</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="header-bottom">
+                    <div class="container">
+                        <div class="header-bottom-menu">
+                            menu
+                        </div>
+                        <div class="header-bottom-nav">
+                            <nav>
+                                <ul>
+                                    <li>
+                                        <a href="#">Межкомнатные двери</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Скрытые двери</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Входные двери</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Фурнитура</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">Где купить</a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                        <div class="header-bottom-action">
+                            <a href="#">Заказать замер</a>
+                        </div>
+                    </div>
+                </div>
             </header>
 
             <main>
