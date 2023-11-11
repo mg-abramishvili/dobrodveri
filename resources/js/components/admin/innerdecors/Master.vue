9126699bed0c5cba133f90b4f4ed31e95b16ddfb
Mikhail Abramishvili <mg@abramishvili.net>
2023-11-11 14:26:58 +0400

    .

--- resources/js/components/admin/innerdecors/Master.vue
+++ resources/js/components/admin/innerdecors/Master.vue
@@ -10,7 +10,9 @@
                             <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"/>
                         </svg>
                     </router-link>
-                    {{ innerdecor.name }}
+
+                    <template v-if="$route.params.id">{{ innerdecor.name }}</template>
+                    <template v-else>Новая внутренняя отделка</template>
                 </h1>
             </div>
         </div>
@@ -50,51 +52,16 @@ export default {
                 loading: true,
                 saveButton: true,
             },
-
-            server: {
-                remove(filename, load) {
-                    load('1');
-                },
-                process: (fieldName, file, metadata, load, error, progress, abort, transfer, options) => {
-                    const formData = new FormData();
-                    formData.append(fieldName, file, file.name);
-                    const request = new XMLHttpRequest();
-                    request.open('POST', '/_admin/file/upload');
-                    request.upload.onprogress = (e) => {
-                        progress(e.lengthComputable, e.loaded, e.total);
-                    };
-                    request.onload = function() {
-                        if (request.status >= 200 && request.status < 300) {
-                            load(request.responseText);
-                        }
-                        else {
-                            error('oh no');
-                        }
-                    };
-                    request.send(formData);
-                    return {
-                        abort: () => {
-                            request.abort();
-                            abort();
-                        }
-                    };
-                },
-                revert: (filename, load) => {
-                    load(filename)
-                },
-                load: (source, load, error, progress, abort, headers) => {
-                    var myRequest = new Request(source);
-                    fetch(myRequest).then(function(response) {
-                        response.blob().then(function(myBlob) {
-                            load(myBlob)
-                        });
-                    });
-                },
-            },
         }
     },
     created() {
-        this.loadInnerDecor()
+        if(this.$route.params.id) {
+            this.loadInnerDecor()
+        }
+        
+        if(!this.$route.params.id) {
+            this.views.loading = false
+        }
     },
     methods: {
         loadInnerDecor() {
@@ -108,7 +75,10 @@ export default {
                 this.views.loading = false
             })
             .catch(errors => {
-                //
+                return this.$swal({
+                    text: errors.response.data ? errors.response.data : errors,
+                    icon: 'error',
+                })
             })
         },
         slugify() {
@@ -132,30 +102,42 @@ export default {
 
             this.views.saveButton = false
 
-            axios.put(`/_admin/innerdecor/${this.$route.params.id}/update`, {
+            let postData = {
                 name: this.name,
-                slug: this.slug
-            })
-            .then(response => {
-                this.views.saveButton = true
-                this.$router.push({ name: 'InnerDecors' })
-            })
-            .catch(errors => {
-                this.views.saveButton = true
+                slug: this.slug,
+            }
 
-                let errorMessage = ''
-
-                if(errors.response.data) {
-                    errorMessage = errors.response.data
-                } else {
-                    errorMessage = errors
-                }
-
-                return this.$swal({
-                    text: errorMessage,
-                    icon: 'error',
+            if(this.$route.params.id) {
+                axios.put(`/_admin/innerdecor/${this.$route.params.id}/update`, postData)
+                .then(response => {
+                    this.views.saveButton = true
+                    this.$router.push({ name: 'InnerDecors' })
                 })
-            })
+                .catch(errors => {
+                    this.views.saveButton = true
+                
+                    return this.$swal({
+                        text: errors.response.data ? errors.response.data : errors,
+                        icon: 'error',
+                    })
+                })
+            }
+
+            if(!this.$route.params.id) {
+                axios.post(`/_admin/innerdecors`, postData)
+                .then(response => {
+                    this.views.saveButton = true
+                    this.$router.push({ name: 'InnerDecors' })
+                })
+                .catch(errors => {
+                    this.views.saveButton = true
+                
+                    return this.$swal({
+                        text: errors.response.data ? errors.response.data : errors,
+                        icon: 'error',
+                    })
+                })
+            }
         },
     },
 }
