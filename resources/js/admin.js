48607706b742c7c4726659880a5d5dbd60447ee3
Mikhail Abramishvili <mg@abramishvili.net>
2023-11-24 15:54:54 +0400

    .

--- resources/js/admin.js
+++ resources/js/admin.js
@@ -12,10 +12,6 @@ import globalFilters from './globalFilters'
 // Loader
 import Loader from './components/admin/Loader.vue'
 
-// SweetAlerts
-import VueSweetalert2 from 'vue-sweetalert2'
-import 'sweetalert2/dist/sweetalert2.min.css'
-
 // Vue Toast Notification
 import ToastPlugin from 'vue-toast-notification'
 import 'vue-toast-notification/dist/theme-default.css'
@@ -26,7 +22,6 @@ import CKEditor from '@ckeditor/ckeditor5-vue'
 const app = createApp(App)
             .component('Loader', Loader)
             .use(Router)
-            .use(VueSweetalert2)
             .use(CKEditor)
             .use(ToastPlugin, {
                 position: 'top'
