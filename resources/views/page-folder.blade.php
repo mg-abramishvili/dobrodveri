774b27dedf0b6ce94f3a8183a0e3dd21e6a9829e
Mikhail Abramishvili <mg@abramishvili.net>
2023-11-26 21:44:27 +0400

    .

--- resources/views/page-folder.blade.php
+++ resources/views/page-folder.blade.php
@@ -20,7 +20,9 @@
             @foreach($pagesF as $page)
                 <a href="/p/{{ $page->slug }}">
                     <div class="page-cover" style="background-image: url({{$page->cover ? $page->cover : '/img/no-image.jpg'}})"></div>
-                    {{ $page->name }}
+                    <div class="page-f-name">
+                        {{ $page->name }}
+                    </div>
                 </a>
             @endforeach
         </div>
