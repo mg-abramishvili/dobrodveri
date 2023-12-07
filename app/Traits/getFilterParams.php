5380c9b0460d3f04333b238adce3c44ef951e6ba
Mikhail Abramishvili <mg@abramishvili.net>
2023-12-07 13:24:16 +0400

    .

--- app/Traits/getFilterParams.php
+++ app/Traits/getFilterParams.php
@@ -20,9 +20,12 @@ trait getFilterParams {
             $filterParams['priceTo'] = isset($req['price_to']) ? (int)$req['price_to'] : null;
             $filterParams['types'] = isset($req['types']) ? $req['types'] : null;
             $filterParams['styles'] = isset($req['styles']) ? $req['styles'] : null;
-            $filterParams['surfaces'] = isset($req['surfaces']) ? $req['surfaces'] : null;
             $filterParams['colors'] = isset($req['colors']) ? $req['colors'] : null;
             $filterParams['glasses'] = isset($req['glasses']) ? $req['glasses'] : null;
+            $filterParams['surfaces'] = isset($req['surfaces']) ? $req['surfaces'] : null;
+            $filterParams['innerdecors'] = isset($req['innerdecors']) ? $req['innerdecors'] : null;
+            $filterParams['purposes'] = isset($req['purposes']) ? $req['purposes'] : null;
+            $filterParams['furnituretypes'] = isset($req['furnituretypes']) ? $req['furnituretypes'] : null;
         }
 
         if($productType == 'product')
@@ -32,9 +35,12 @@ trait getFilterParams {
             $filterParams['priceTo'] = $request->price_to;
             $filterParams['types'] = $request->type ? explode(',', $request->type) : null;
             $filterParams['styles'] = $request->style ? explode(',', $request->style) : null;
-            $filterParams['surfaces'] = $request->surface ? explode(',', $request->surface) : null;
             $filterParams['colors'] = $request->color ? explode(',', $request->color) : null;
             $filterParams['glasses'] = $request->glass ? explode(',', $request->glass) : null;
+            $filterParams['surfaces'] = $request->surface ? explode(',', $request->surface) : null;
+            $filterParams['innerdecors'] = $request->innerdecor ? explode(',', $request->innerdecor) : null;
+            $filterParams['purposes'] = $request->purpose ? explode(',', $request->purpose) : null;
+            $filterParams['furnituretypes'] = $request->furnituretype ? explode(',', $request->furnituretype) : null;
         }
 
         return $filterParams;
