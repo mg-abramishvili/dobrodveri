f43796ddb7c52ee319614dffa1514ffb3aba667f
Mikhail Abramishvili <mg@abramishvili.net>
2023-12-08 12:56:44 +0400

    .

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -21,7 +21,7 @@ class Product extends Model
 
     public function sizes()
     {
-        return $this->belongsToMany(Size::class);
+        return $this->belongsToMany(Size::class)->orderBy('slug');
     }
 
     public function style()
